03a1dbedb54328ee19c4a8a131f24579c2d1cfa9
ariefmavlana <ariefmaulana0808@gmail.com>
2025-08-08 06:19:15 +0700

    gsap-three-planet-comm

---
+++ README.md
@@ -0,0 +1,22 @@
+# 🌐 3D Arief Maulana Portfolio
+
+### Built with React, GSAP, Three.js, TailwindCSS
+
+This is a fully animated, interactive, 3D developer portfolio designed to you **clients, recruiters, and hiring managers**. It's more than a portfolio—it's a web experience built with production-level code, scroll-based animations, and real-world best practices.
+
+> ⚡ Inspired by Awwwards-level sites — built with React (Vite), TailwindCSS, GSAP, React Three Fiber, and Drei.
+
+---
+
+## 🚀 Tech Stack
+
+| Technology       | Description                             |
+| ---------------- | --------------------------------------- |
+| **React (Vite)** | Fast dev server and production bundling |
+| **Tailwind CSS** | Utility-first styling for components    |
+| **GSAP**         | Scroll-based animation and motion logic |
+| **Three.js**     | 3D scenes powered by React Three Fiber  |
+| **Drei**         | Useful helpers for 3D rendering         |
+
+---
+
---
+++ src/constants/index.js
@@ -0,0 +1,187 @@
+// index.js
+export const servicesData = [
+  {
+    title: "FullStack Development",
+    description:
+      "Your business deserves a fast, secure, and future-proof digital foundation. I develop custom web apps with clean architecture, optimized databases, and seamless integrations—ensuring reliability at every layer.",
+    items: [
+      {
+        title: "Backend Engineering",
+        description: "(REST/GraphQL APIs, Microservices, Auth Systems)",
+      },
+      {
+        title: "Frontend Excellence",
+        description: "(React, Vue, TypeScript, Interactive UI/UX)",
+      },
+      {
+        title: "Database Design",
+        description: "(SQL/NoSQL Optimization, Scalable Structures)",
+      },
+    ],
+  },
+  {
+    title: "DevOps & Cloud Solutions",
+    description:
+      "Deploying software shouldn't be a gamble. I automate infrastructure, enforce security, and leverage cloud platforms (AWS/Azure) to keep your app running smoothly—24/7, at any scale.",
+    items: [
+      {
+        title: "CI/CD Pipelines",
+        description: "(GitHub Actions, Docker, Kubernetes)",
+      },
+      {
+        title: "Server Management ",
+        description: "(Linux, Nginx, Load Balancing)",
+      },
+      {
+        title: "Performance Tuning",
+        description: "(Caching, Compression, Lighthouse 90+ Scores)",
+      },
+    ],
+  },
+  {
+    title: "Security & Optimization",
+    description:
+      "Slow or hacked apps destroy trust. I harden security (XSS/SQLI protection, OAuth) and optimize bottlenecks so your app stays fast, safe, and scalable as you grow.",
+    items: [
+      {
+        title: "Code Audits",
+        description: "(Refactoring, Tech Debt Cleanup)",
+      },
+      {
+        title: "Pen Testing",
+        description: "(Vulnerability Assessments)",
+      },
+      {
+        title: "SEO Tech Stack",
+        description: "(SSR, Metadata, Structured Data)",
+      },
+    ],
+  },
+  {
+    title: "Web & Mobile Apps",
+    description:
+      "A clunky interface can sink even the best ideas. I craft responsive, pixel perfect web and mobile apps (React Native/Flutter) that users love—bridging design and functionality seamlessly.",
+    items: [
+      {
+        title: "Cross-Platform Apps",
+        description: "(Single codebase for iOS/Android/Web)",
+      },
+      {
+        title: "PWAs",
+        description: "(Offline mode, Push Notifications)",
+      },
+      {
+        title: "E-Commerce",
+        description: "(Checkout flows, Payment Gateways, Inventory APIs)",
+      },
+    ],
+  },
+]
+
+export const projects = [
+  {
+    id: 1,
+    name: "Car Dealer Website",
+    description:
+      "An online car dealer specializing in motors sport or daily.",
+    href: "",
+    image: "/assets/projects/mobile-accessories-store.jpg",
+    bgImage: "/assets/backgrounds/blanket.jpg",
+    frameworks: [
+      { id: 1, name: "Next" },
+      { id: 2, name: "PostgreSQL" },
+      { id: 3, name: "Prisma" },
+      { id: 4, name: "AWS" },
+      { id: 5, name: "Tailwind CSS" },
+    ],
+  },
+  {
+    id: 2,
+    name: "3D Personal Portfolio",
+    description:
+      ".",
+    href: "",
+    image: "/assets/projects/plant-shop.jpg",
+    bgImage: "/assets/backgrounds/curtains.jpg",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "Tailwind CSS" },
+      { id: 3, name: "Three.js" },
+      { id: 4, name: "GSAP" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Apple Tech Marketplace",
+    description:
+      "An e-commerce platform for Apple products and accessories with deals and category filtering.",
+    href: "",
+    image: "/assets/projects/apple-tech-store.jpg",
+    bgImage: "/assets/backgrounds/map.jpg",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "Redux" },
+      { id: 3, name: "PostgreSQL" },
+      { id: 4, name: "Prisma" },
+      { id: 5, name: "Tailwind CSS" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Inventory Management Electronics & Gadgets Store",
+    description:
+      "A multi-category online shop featuring electronics, home appliances, and gaming gear with special offers.",
+    href: "",
+    image: "/assets/projects/electronics-store.jpg",
+    bgImage: "/assets/backgrounds/poster.jpg",
+    frameworks: [
+      { id: 1, name: "Next" },
+      { id: 2, name: "AWS" },
+      { id: 3, name: "PostgreSQL" },
+      { id: 4, name: "Prisma" },
+      { id: 5, name: "Tailwind CSS" },
+    ],
+  },
+  {
+    id: 5,
+    name: "Real Estate",
+    description:
+      "An advanced search for searching Real Estate in your favourite place.",
+    href: "",
+    image: "/assets/projects/home-decor-store.jpg",
+    bgImage: "/assets/backgrounds/table.jpg",
+    frameworks: [
+      { id: 1, name: "Next" },
+      { id: 2, name: "AWS" },
+      { id: 3, name: "PostgreSQL" },
+      { id: 4, name: "Prisma" },
+      { id: 5, name: "Shadcn UI" },
+    ],
+  },
+  {
+    id: 6,
+    name: "Project Management Mavl",
+    description:
+      "A project management tool for teams.",
+    href: "",
+    image: "/assets/projects/game-store.jpg",
+    bgImage: "/assets/backgrounds/curtains.jpg",
+    frameworks: [
+      { id: 1, name: "Next" },
+      { id: 2, name: "AWS" },
+      { id: 3, name: "PostgreSQL" },
+      { id: 4, name: "Prisma" },
+      { id: 5, name: "Shadcn UI" },
+    ],
+  },
+]
+
+export const socials = [
+  { name: "Instagram", href: "https://www.instagram.com/" },
+  {
+    name: "Youtube",
+    href: "https://www.youtube.com/",
+  },
+  { name: "LinkedIn", href: "https://www.linkedin.com/" },
+  { name: "GitHub", href: "https://github.com/" },
+]
